c3566c0097426064f199dfd375e1adf8b2f29a49
Kyle Claravall <62874933+kyleclaravall@users.noreply.github.com>
2020-10-16 20:05:29 +1100

    Update make_bed.py

--- scripts/make_bed.py
+++ scripts/make_bed.py
@@ -2,12 +2,15 @@
 
 import pandas as pd
 
+# read in table 9
 raw_df = pd.read_csv('t9.csv', sep=',', header=2)
 subset_df = raw_df[raw_df['LOCUS'] == 'IQCK']
 
+# set output file
 outFileName = 'IQCK_variants.BED'
 outFile = open(outFileName, 'w+')
 
+# create bed_list
 scaffold = 'NC_000016.9'
 bed_list = []
 
@@ -20,10 +23,11 @@ for pos in subset_df['Pos']:
     index += 1
     bed_list.append(string)
 
-
+# repeat for table 11
 raw2 = pd.read_csv('t11.csv', sep=',', header=0)
 subset2 = raw2[raw2['Locus'] == 'IQCK']
 
+# reset index counter
 index = 0
 for pos in subset2['Pos']:
     string = '{}'.format(scaffold)
@@ -33,8 +37,9 @@ for pos in subset2['Pos']:
     index += 1
     bed_list.append(string)
 
-
+# remove duplicate entries in bed_list
 final_list = list(dict.fromkeys(bed_list))
 
+# write each entry in bed_list to output file (.bed file)
 for i in final_list:
     outFile.write(i)
